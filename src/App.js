9c9dd1466dbd12a6830ff49101eb158d4474a1eb
matthewraymondh <raymondhartono76@gmail.com>
2021-11-29 00:06:34 +0700

    react js porto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import { Route, Switch, useLocation } from "react-router"
+import { ThemeProvider } from "styled-components"
+import { lightTheme } from "./components/Themes"
+import GlobalStyle from "./globalStyles"
+
+
+//components
+import Main from './components/Main';
+import AboutPage from './components/AboutPage';
+import BlogPage from './components/BlogPage';
+import WorkPage from './components/WorkPage';
+import MySkillsPage from './components/MySkillsPage';
+import { AnimatePresence } from "framer-motion";
+import SoundBar from "./subComponents/SoundBar";
+
 function App() {
-  return <div>
-    App File
-    </div>
+
+  const location = useLocation();
+  return <>
+
+
+  <GlobalStyle />
+
+    <ThemeProvider theme={lightTheme}>
+
+    <SoundBar/>
+
+    <AnimatePresence exitBeforeEnter>
+    <Switch location={location} key={location.pathname}>
+      <Route exact path="/" component={Main}/>
+      <Route exact path="/about" component={AboutPage}/>
+      <Route exact path="/blog" component={BlogPage}/>
+      <Route exact path="/work" component={WorkPage}/>
+      <Route exact path="/skills" component={MySkillsPage}/>
+      
+    </Switch>
+    </AnimatePresence>
+    
+
+    </ThemeProvider>
+
+
+    
+    </>
     
 }
 
