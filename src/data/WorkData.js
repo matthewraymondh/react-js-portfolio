036ebfd3c115deb16890628387ddfa7472c1a3b1
Matthew Raymond <83885819+matthewraymondh@users.noreply.github.com>
2021-12-06 17:53:26 +0700

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -3,10 +3,19 @@ export const Work = [
         id:1,
         name:"InstaClone",
         description:"Instagram Clone build on react native and firebase still prototype.",
-        tags:["react","firebase","JavaScript"],
+        tags:["react-native","firebase","JavaScript"],
         
         demo:"https://expo.dev/@matthewraymond/Instaclone",
         github:"https://github.com/matthewraymondh/instaclone-react-native"
+    },
+    {
+        id:2,
+        name:"Filmoon",
+        description:"A movie streaming website prototype built on react-js using TMDB API",
+        tags:["react-js","firebase","JavaScript", "API"],
+        
+        demo:"https://netclone-14d78.web.app/",
+        github:"https://github.com/matthewraymondh/Filmoon"
     },
     
     
