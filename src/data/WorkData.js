43acacc570a138f81f518ce663b7d2bbfddb9577
Matthew Raymond <83885819+matthewraymondh@users.noreply.github.com>
2021-12-09 10:40:46 +0700

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -11,8 +11,8 @@ export const Work = [
     {
         id:2,
         name:"Filmoon",
-        description:"A movie streaming website prototype built on react-js using TMDB API",
-        tags:["react-js","firebase","JavaScript", "API"],
+        description:"A movie streaming website prototype built on react-js using TMDB API, react-router for seamless navigation and redux to power user authentication",
+        tags:["react-js","firebase","JavaScript", "API", "Redux", "react-router"],
         
         demo:"https://netclone-14d78.web.app/",
         github:"https://github.com/matthewraymondh/Filmoon"
